82bf5f6b18b49570ec49f5ad269cab489173fb65
Javitodev <javito.dev@gmail.com>
2024-07-25 19:16:25 -0400

    barrels updated

--- src/cebra/components/index.ts
+++ src/cebra/components/index.ts
@@ -1,5 +1,8 @@
-import LinkCard from "./LinkCard.astro"
+import LinkCard from './LinkCard.astro'
+import BrandText from './BrandText.astro'
+import LinkButton from './LinkButton.astro'
+import Accent from './Accent.astro'
+import Container from './Container.astro'
+import MethodCard from './MethodCard.astro'
 
-export {
-  LinkCard
-}
+export { LinkCard, BrandText, LinkButton, Accent, Container, MethodCard }
--- src/cebra/modules/index.ts
+++ src/cebra/modules/index.ts
@@ -1,4 +1,7 @@
+import BannerCebras from './BannerCebras.astro'
+import BannerContact from './BannerContact.astro'
 import Hero from './Hero.astro'
+import OurMethod from './OurMethod.astro'
 import Services from './Services.astro'
 
-export { Hero, Services }
+export { Hero, Services, BannerContact, BannerCebras, OurMethod }
